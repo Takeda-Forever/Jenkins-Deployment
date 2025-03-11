a503f64d331c1091bcb3fda9c1acb13323c75350
Takeda-Forever <85308264+Takeda-Forever@users.noreply.github.com>
2025-03-11 18:03:23 +0500

    Update init_check.bat

--- init_check.bat
+++ init_check.bat
@@ -1,12 +1,15 @@
 @echo off
-echo ===== Pipeline Info =====
-echo Job Name: %JOB_NAME%
-echo Build Number: %BUILD_NUMBER%
-echo Build ID: %BUILD_ID%
-echo Workspace: %WORKSPACE%
-echo Git Branch: %GIT_BRANCH%
-echo Git Commit: %GIT_COMMIT%
-echo Node Name: %NODE_NAME%
-echo Executor: %EXECUTOR_NUMBER%
-echo Jenkins URL: %JENKINS_URL%
-echo ===================================
+echo ==============================================
+echo 🚀  Информация о пайплайне CI/CD! 🚀
+echo ==============================================
+echo 📌 Job Name:       %JOB_NAME%
+echo 🔢 Build Number:   %BUILD_NUMBER%
+echo 🆔 Build ID:       %BUILD_ID%
+echo 📂 Workspace:      %WORKSPACE%
+echo 🌿 Git Branch:     %GIT_BRANCH%
+echo 🔗 Git Commit:     %GIT_COMMIT%
+echo 🖥  Node Name:      %NODE_NAME%
+echo 🏃 Executor:       %EXECUTOR_NUMBER%
+echo 🌐 Jenkins URL:    %JENKINS_URL%
+echo ==============================================
+echo ✅  Пайплайн успешно стартует! 🚀
