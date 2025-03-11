b47752b91320bf97edadefb2cec6c36a3bf80f86
Takeda-Forever <85308264+Takeda-Forever@users.noreply.github.com>
2025-03-11 17:10:25 +0500

    Update run_docker.bat

--- run_docker.bat
+++ run_docker.bat
@@ -1,3 +1,3 @@
-docker stop ${CONTAINER_NAME} || exit 0
-docker rm ${CONTAINER_NAME} || exit 0
-docker run -d --name ${CONTAINER_NAME} -p 8080:80 ${DOCKER_IMAGE}
+docker stop %CONTAINER_NAME% || exit 0
+docker rm %CONTAINER_NAME% || exit 0
+docker run -d --name %CONTAINER_NAME% -p 8080:80 %DOCKER_IMAGE%
